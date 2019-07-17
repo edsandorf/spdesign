97b5b1684bd9f913dbf34b7efc8f2874899a231e
edsandorf <esandorf@gmail.com>
2019-07-17 08:50:02 +0100

    Updated title and description to be CRAN compliant

--- R/spdesignR.R
+++ R/spdesignR.R
@@ -1,6 +1,9 @@
-#' spdesignR: Stated Preference Designs in R
+#' spdesignR: Designing Stated Preference Experiments in R
 #'
-#' spdesignR makes it easy to create designs for stated preference experiments.
+#' Contemporary software commonly used to design stated preference experiments
+#' are expensive and the code is closed source. spdesignR is a free software
+#' package with an easy to use interface to make flexible stated preference
+#' experimental designs using state-of-the-art methods.
 #'
 #' @import stringr
 #'
