01af6c413b2e5c293dd2506804ce44768f275c5c
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-01-15 14:36:53 +0100

    Added warnings, errors and tests for is_balanced

--- R/utils.R
+++ R/utils.R
@@ -8,8 +8,22 @@
 #' @param clos A closing bracket ) ] or >
 #'
 #' @noRd
-
 is_balanced <- function(string, open, close) {
+  opening <- c("(", "[", "<", "{")
+  closing <- c(")", "]", ">", "}")
+
+  if (!(open %in% opening)) {
+    stop("The function only supports the following opening brackets: '(', '[', '<' ann '{'")
+  }
+
+  if (!(close %in% closing)) {
+    stop("The function only supports the following closing brackets: ')', ']', '>' and '}")
+  }
+
+  if (grep(paste0("\\", open), opening) != grep(paste0("\\", close), closing)) {
+    warning("The opening and closing brackets do not match. This will very likely result in an error and the function evaluating to FALSE, but left in because this unintended consequence might be useful.\n")
+  }
+
   opened <- str_count(string, paste0("\\", open))
   closed <- str_count(string, paste0("\\", close))
   if (opened != closed) {
