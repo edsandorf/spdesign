d75d4389e144ac09356aac919281d885d813746d
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-01-22 15:03:02 +0100

    Updated to a very simple MNL design example.

--- examples/mnl-design.R
+++ examples/mnl-design.R
@@ -2,10 +2,12 @@
 # Example file for creating a simple MNL design
 #
 
+library(spdesign)
+
 # Set design options ----
 opts <- list(
   optimization_algorithm = "random", # "random", "federov", "rsc"
-  efficiency_criteria = "d-efficiency",
+  efficiency_criteria = "d_efficiency",
   model = "mnl",
   blocks = 1,
   tasks = 6,
@@ -14,9 +16,9 @@ opts <- list(
 
 # Define the list of utility functions ----
 V <- list(
-  alt1 = "b_x1[0.1] * x_1[2:5] + b_x2[0.4] * x_2[c(0, 1)] + b_x3[Np(0, 1)] * x_3[seq(0, 1, 0.25)]",
+  alt1 = "b_x1[0.1] * x_1[2:5] + b_x2[0.4] * x_2[c(0, 1)] + b_x3[-0.2] * x_3[seq(0, 1, 0.25)]",
   alt2 = "b_x1      * x_1      + b_x2      * x_2          + b_x3          * x_3"
 )
 
 # Generate designs ----
-design(V, opts)
+generate_design(V, opts)
