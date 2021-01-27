d18447a09106170716c02b6355b531604ff76a1b
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-01-27 13:41:08 +0100

    Added functions for transforming distributions

--- R/distributions.R
+++ R/distributions.R
@@ -74,9 +74,52 @@ U <- function(mu, sigma) {
 #' @describeIn N The uniform distribution when applied to a prior
 Up <- U
 
+
+
+#' Transform distribution
+#'
+#' @param mu A value for the mean of the distribution
+#' @param sigma A value for the standard deviation of the distribution
+#' @param eta A numeric standard uniform vector
+#' @param type The type of distribution
+#'
+#' @return A vector with the transformed distribution given the parameters
+transform_distribution <- function(mu, sigma, eta, type) {
+  switch(
+    type,
+    N = transform_n(mu, sigma, eta),
+    LN = transform_ln(mu, sigma, eta),
+    U = transform_u(mu, sigma, eta),
+    TR = transform_tr(mu, sigma, eta),
+  )
+}
+
 #' Transform to the normal distribution
 #'
-#' @param x A numeric standard uniform vector
-transform_n <- function(x) {
-  qnorm(x)
+#' @inheritParams transform_distribution
+transform_n <- function(mu, sigma, eta) {
+  mu + sigma * qnorm(eta)
+}
+
+#' Transform to the lognormal distribution
+#'
+#' @inheritParams transform_distribution
+transform_ln <- function(mu, sigma, eta) {
+  exp(mu + sigma * qnorm(eta))
+}
+
+#' Transform to the uniform distribution
+#'
+#' @inheritParams transform_distribution
+transform_u <- function(mu, sigma, eta) {
+  mu + sigma * (2 * eta - 1)
+}
+
+#' Transform to the triangular distribution
+#'
+#' @inheritParams transform_distribution
+transform_tr <- function(mu, sigma, eta) {
+  idx <- +(eta < 0.5)
+  eta <- idx * (sqrt(2 * eta) - 1) + (1 - idx) * (1 - sqrt(2 * (1 - eta)))
+  mu + sigma * eta
 }
---
+++ tests/testthat/test-distributions.R
@@ -0,0 +1,19 @@
+context("Test the transformation of distributions")
+
+eta <- make_draws(1, 2, 1, 123, "scrambled-sobol")
+
+test_that("Normal distribution", {
+  expect_equal(transform_distribution(0, 1, eta, "N"), structure(c(0.902109721985119, -0.239394921333748), .Dim = 2:1))
+})
+
+test_that("Lognormal distribution", {
+  expect_equal(transform_distribution(0, 1, eta, "LN"), structure(c(2.46479766752339, 0.787103976832784), .Dim = 2:1))
+})
+
+test_that("Normal distribution", {
+  expect_equal(transform_distribution(0, 1, eta, "U"), structure(c(0.63300141505897, -0.189200632274151), .Dim = 2:1))
+})
+
+test_that("Normal distribution", {
+  expect_equal(transform_distribution(0, 1, eta, "TR"), structure(c(0.394195918682426, -0.0995560163309163), .Dim = 2:1))
+})
