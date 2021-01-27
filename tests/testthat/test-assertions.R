6fdad1979421f473eb45179c6065630516913dae
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-01-27 13:47:59 +0100

    Added tests for assertions.

---
+++ tests/testthat/test-assertions.R
@@ -0,0 +1,9 @@
+context("Test assertions")
+
+test_that("is_balance returns TRUE/FALSE in correct cases", {
+  expect_true(is_balanced("N()", "(", ")"))
+  expect_true(is_balanced("b_x1[Np(0, 1), Np(0, 1)]", "(", ")"))
+  expect_false(is_balanced("b_x1[Np(0, 1), Np(0, 1]", "(", ")"))
+  expect_true(is_balanced("b_x1[Np(0, 1), Np(0, 1)]", "[", "]"))
+  expect_warning(is_balanced("b_x1[Np(0, 1), Np(0, 1)]", "(", "]"))
+})
