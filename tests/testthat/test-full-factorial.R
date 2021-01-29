ddea61ef1e56858aeec072025ee042a711a8f142
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-01-29 13:36:34 +0100

    Added test cases

--- tests/testthat/test-full-factorial.R
+++ tests/testthat/test-full-factorial.R
@@ -1,3 +1,7 @@
 context("Test the dimensions and range of the full factorial")
 
-# test_that("")
+test_that("Full factorial retrieves correct dimensions", {
+  expect_equal(dim(generate_full_factorial(list(attr1 = c(0, 1), attr2 = c(0, 1)))), c(4, 2))
+  expect_equal(dim(generate_full_factorial(list(attr1 = c(0, 1, 3), attr2 = c(0, 1, 3), attr3 = c(0, 1)))), c(18, 3))
+  expect_equal(dim(generate_full_factorial(list(attr1 = c(0, 1, 3), attr2 = c(0, 1, 3), attr3 = c(0, 1, 2)))), c(27, 3))
+})
