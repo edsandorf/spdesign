58a13a6edd1e4db6151d7e3422a799b8bcefd886
Erlend Dancke Sandorf <edsandorf@gmail.com>
2023-05-30 13:28:00 +0200

    New and updated function for getting attribute level occurrence.

--- R/methods-utility.R
+++ R/methods-utility.R
@@ -152,11 +152,21 @@ priors <- function(x) {
 
 #' Generic for extracting the attribute occurrence
 #'
-#' @inheritParams priors
+#' The function is called from within \code{\link{parse_utility}} with the
+#' purpose of getting the list of attributes, levels and occurrences within the
+#' design. This is necessary to impose attribute level balance or near attribute
+#' level balance. This is also necessary to correctly populate the candidate
+#' matrix used for the RSC algorithm.
 #'
+#' @inheritParams priors
+#' @param rows Number of rows in the design
+#'
+#' @return A named list of lists where the outer list is for the attributes and
+#' the inner list, the levels of each attribute and the number or range of times
+#' they can occur
 #'
 #' @export
-occurrences <- function(x) {
+occurrences <- function(x, rows = NULL) {
   # Check the class of 'x' to return early if 'spdesign'
   if ("spdesign" %in% class(x)) {
     return(
@@ -165,5 +175,114 @@ occurrences <- function(x) {
   }
 
   # Extract level occurrence
+  attribute_lvls <- expand_attribute_levels(x)
+  n_lvls <- lapply(attribute_lvls, length)
+
+  # Set the default level occurrence assuming utility balance.
+  level_occurrences <- lapply(n_lvls, function(n) {
+    # If unbalanced and unspecified, set minimum range for occurrence
+    if (rows %% n == 0) {
+      lvl <- rows / n
+
+    } else {
+      minimum <- floor(rows / n)
+      lvl <- minimum:(minimum + 1)
+
+    }
+
+    lvls <- lapply(seq_len(n), function(i) lvl)
+    names(lvls) <- paste0("lvl", seq_along(lvls))
+
+    return(lvls)
+  })
+
+  # Create a list to populate with occurrences
+  specified_values <- extract_specified(x, simplify = TRUE)
+  idx <- str_detect(specified_values, "(?<=\\])\\(.*?\\)")
+  occurrences <- vector(mode = "list", sum(idx))
+  names(occurrences) <- extract_attribute_names(specified_values[idx])
+
+  # Transform the level occurrences to a list and if a single value, expand.
+  for (i in seq_along(occurrences)) {
+    occurrence <- extract_level_occurrence(specified_values[idx][[i]])
+    lvls <- eval(parse(text = paste0("list", occurrence)))
+
+    # Check if it only occurs once and repeat equal to the number of levels
+    if (length(lvls) == 1) {
+      # DANGER HERE: We are only using the first match. This will mean that
+      # different levels for the same attribute in different alternatives won't
+      # work.
+      lvls <- rep(lvls, length(attribute_lvls[[grep(paste0("\\b.*?", names(occurrences)[[i]]), names(attribute_lvls))[[1]]]]))
+    }
+
+    names(lvls) <- paste0("lvl", seq_along(lvls))
+    occurrences[[i]] <- lvls
+
+  }
+
+  # Expand to the wide format
+  occurrences <- lapply(seq_along(occurrences), function(i) {
+    occurrences_j <- lapply(seq_along(x), function(j) {
+      if (str_detect(x[[j]], names(occurrences[i]))) {
+        occurrences_tmp <- occurrences[i]
+        names(occurrences_tmp) <- paste(names(x[j]), names(occurrences[i]), sep = "_")
+
+        return(occurrences_tmp)
+
+      }
+    })
+
+    return(
+      do.call(c, occurrences_j)
+    )
+  })
+
+  occurrences <- do.call(c, occurrences)
+
+  # Replace the default values with the specified level occurrence
+  level_occurrences[names(occurrences)] <- occurrences
+
+  return(
+    level_occurrences
+  )
 
 }
+
+
+# Checks within the old level-occurrence function
+# # Test that occurrence is specified once or equal to the number of levels
+# n_lvl_occurrence <- lapply(level_occurrence, length)
+# length_diff <- mapply(
+#   "-",
+#   n_attr_lvls,
+#   n_lvl_occurrence,
+#   SIMPLIFY = TRUE
+# )
+#
+# # Check that the attribute level is only specified equal to the
+# if (any(length_diff != 0)) {
+#   stop(
+#     "The number of level occurrences specified can only be of length one or
+#         equal to the number of levels for the attribute"
+#   )
+# }
+#
+# tmp <- lapply(level_occurrence, function(x) {
+#   sum_min <- Reduce("+", lapply(x, min))
+#   sum_max <- Reduce("+", lapply(x, max))
+#   !(candidate_rows %in% sum_min:sum_max)
+# })
+#
+# if (any(do.call(c, tmp))) {
+#   stop(
+#     "The specified ranges for the attribute level occurrence or the number of
+#       times an attribute level should occur does not allow you to find a design.
+#       The number of rows in the design candidate must be either equal to the
+#       sum of individual occurrences of an attribute level or contained in the
+#       range of the sum of the minimum number of occurrences and the sum of the
+#       maximum number of level occurrences."
+#   )
+# }
+#
+# # Implicitly return the list of level occurrences ----
+# level_occurrence
--- tests/testthat/test-level-occurrence.R
+++ tests/testthat/test-level-occurrence.R
@@ -2,25 +2,22 @@ context("Test the function getting level occurrence")
 
 test_that("Test 1", {
   utility <- list(
-    alt1 = "b_x1[0.1] * x_1[2:5]  + b_x2[0.4] * x_2[c(0, 1)]+ b_x3[-0.2] * x_3[seq(0, 1, 0.25)]",
-    alt2 = "b_x1      * x_1             + b_x3          * x_3"
+    alt1 = "b_x1[0.1] * x1[2:5]  + b_x2[0.4] * x2[c(0, 1)]+ b_x3[-0.2] * x3[seq(0, 1, 0.25)]",
+    alt2 = "b_x1      * x1             + b_x3          * x3"
   )
 
-  attrs <- list(
-    x1 = 2:5,
-    x2 = c(0, 1),
-    x3 = seq(0, 1, 0.25)
-  )
-
-  candidate_rows <- 12
+  rows <- 12
 
   expect_equal(
     suppressWarnings(
-      get_level_occurrence(utility, attrs, candidate_rows)
+      occurrences(utility, rows)
     ),
-    list(x1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3), x2 = list(
-      lvl1 = 6, lvl2 = 6), x3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3,
-                                     lvl4 = 2:3, lvl5 = 2:3))
+    list(alt1_x1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3),
+         alt1_x2 = list(lvl1 = 6, lvl2 = 6), alt1_x3 = list(lvl1 = 2:3,
+                                                            lvl2 = 2:3, lvl3 = 2:3, lvl4 = 2:3, lvl5 = 2:3), alt2_x1 = list(
+                                                              lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3), alt2_x2 = list(
+                                                                lvl1 = 12), alt2_x3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3,
+                                                                                           lvl4 = 2:3, lvl5 = 2:3))
   )
 })
 
@@ -31,22 +28,18 @@ test_that("Test 2", {
     alt2 = "b_x1      * x_1             + b_x3          * x_3"
   )
 
-  attrs <- list(
-    x1 = 2:5,
-    x3 = seq(0, 1, 0.25),
-    x2 = c(0, 1)
-  )
-
-  candidate_rows <- 12
+  rows <- 12
 
   expect_equal(
     suppressWarnings(
-      get_level_occurrence(utility, attrs, candidate_rows)
+      occurrences(utility, rows)
     ),
-    list(x1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3),
-         x3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3,
-                   lvl4 = 2:3, lvl5 = 2:3),
-         x2 = list(    lvl1 = 6, lvl2 = 6))
+    list(alt1_x_1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3),
+         alt1_x_3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3, lvl4 = 2:3,
+                         lvl5 = 2:3), alt1_x_2 = list(lvl1 = 6, lvl2 = 6), alt2_x_1 = list(
+                           lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3), alt2_x_3 = list(
+                             lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3, lvl4 = 2:3, lvl5 = 2:3),
+         alt2_x_2 = list(lvl1 = 12))
   )
 })
 
@@ -57,64 +50,19 @@ test_that("Test 3", {
     alt2 = "b_x1      * x_1             + b_x3          * x_3 + b_x2 * x_2[c(0, 1)]"
   )
 
-  attrs <- list(
-    x1 = 2:5,
-    x3 = seq(0, 1, 0.25),
-    x2 = c(0, 1)
-  )
 
-  candidate_rows <- 12
+  rows <- 12
 
   expect_equal(
     suppressWarnings(
-      get_level_occurrence(utility, attrs, candidate_rows)
+      occurrences(utility, rows)
     ),
-    list(x1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3),
-         x3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3,
-                   lvl4 = 2:3, lvl5 = 2:3),
-         x2 = list(    lvl1 = 6, lvl2 = 6))
+    list(alt1_x_1 = list(lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3),
+         alt1_x_3 = list(lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3, lvl4 = 2:3,
+                         lvl5 = 2:3), alt1_x_2 = list(lvl1 = 6, lvl2 = 6), alt2_x_1 = list(
+                           lvl1 = 3, lvl2 = 3, lvl3 = 3, lvl4 = 3), alt2_x_3 = list(
+                             lvl1 = 2:3, lvl2 = 2:3, lvl3 = 2:3, lvl4 = 2:3, lvl5 = 2:3),
+         alt2_x_2 = list(lvl1 = 6, lvl2 = 6))
   )
 })
 
-test_that("Test 4", {
-  utility <- list(
-    alt1 = "b_x1[0.1] * x1[2:5](3)  +  b_x3[-0.2] * x3[seq(0, 1, 0.25)] + b_x2[0.4] * x2",
-    alt2 = "b_x1      * x1             + b_x3          * x3 + b_x2 * x2[c(0, 1)](1:4)"
-  )
-
-  attrs <- list(
-    x1 = 2:5,
-    x3 = seq(0, 1, 0.25),
-    x2 = c(0, 1)
-  )
-
-  candidate_rows <- 12
-
-  expect_error(
-    suppressWarnings(
-      get_level_occurrence(utility, attrs, candidate_rows)
-    )
-  )
-})
-
-test_that("Test 5", {
-  utility <- list(
-    alt1 = "b_x1[0.1] * x1[2:5](3,  3, 3)  +  b_x3[-0.2] * x3[seq(0, 1, 0.25)] + b_x2[0.4] * x2",
-    alt2 = "b_x1      * x1             + b_x3          * x3 + b_x2 * x2[c(0, 1)](5:7, 4:8)"
-  )
-
-  attrs <- list(
-    x1 = 2:5,
-    x3 = seq(0, 1, 0.25),
-    x2 = c(0, 1)
-  )
-
-  candidate_rows <- 12
-
-  expect_error(
-    suppressWarnings(
-      get_level_occurrence(utility, attrs, candidate_rows)
-    )
-  )
-
-})
