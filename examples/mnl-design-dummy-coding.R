aaa62279d8462a30c7174a0746d448a6d6cdd2ca
Erlend Dancke Sandorf <edsandorf@gmail.com>
2023-06-12 09:22:09 +0200

    Updated examples

--- examples/mnl-design-dummy-coding.R
+++ examples/mnl-design-dummy-coding.R
@@ -5,14 +5,16 @@ rm(list = ls(all = TRUE))
 # library(spdesign)
 
 # Define the list of utility functions ----
+#' A design where the first attribute is dummy-coded.
 utility <- list(
-  alt1 = "b_x1_dummy[c(0.1, 0.2)] * x1[c(1, 3, 5)] + b_x2[0.4] * x2[c(0, 1)] + b_x3[-0.2] * x3[seq(0, 1, 0.25)]",
+  alt1 = "b_x1_dummy[c(0.1, 0.2)] * x1[c(1, 2, 3)] + b_x2[0.4] * x2[c(0, 1)] + b_x3[-0.2] * x3[seq(0, 1, 0.25)]",
   alt2 = "b_x1_dummy              * x1             + b_x2      * x2          + b_x3       * x3"
 )
 
 # Generate designs ----
 design <- generate_design(utility, rows = 20,
                           model = "mnl", efficiency_criteria = "d-error",
-                          algorithm = "random", draws = "scrambled-sobol")
+                          algorithm = "rsc", draws = "scrambled-sobol")
 
+# Add a blocking variable to the design with 2 blocks.
 design <- block(design, 2)
--- examples/mnl-design.R
+++ examples/mnl-design.R
@@ -5,14 +5,21 @@ rm(list = ls(all = TRUE))
 # library(spdesign)
 
 # Define the list of utility functions ----
+#' Specifying a utility function with 3 attributes and a constant for the
+#' SQ alternative. The design has 20 rows.
 utility <- list(
-  alt1 = "b_x1[0.1] * x_1[1:5] + b_x2[0.4] * x_2[c(0, 1)] + b_x3[-0.2] * x_3[seq(0, 1, 0.25)]",
-  alt2 = "b_x1      * x_1      + b_x2      * x_2          + b_x3          * x_3"
+  alt1 = "b_x1[0.1]  * x1[1:5] + b_x2[0.4] * x2[c(0, 1)] + b_x3[-0.2] * x3[seq(0, 1, 0.25)]",
+  alt2 = "b_x1       * x1      + b_x2      * x2          + b_x3       * x3",
+  alt3 = "b_sq[0.15] * sq[1]"
 )
 
 # Generate designs ----
 design <- generate_design(utility, rows = 20,
                           model = "mnl", efficiency_criteria = "d-error",
-                          algorithm = "federov", draws = "scrambled-sobol")
+                          algorithm = "rsc", draws = "scrambled-sobol",
+                          control = list(
+                            max_iter = 21000
+                          ))
 
+# Add a blocking variable to the design with 4 blocks.
 design <- block(design, 4)
